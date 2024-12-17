d1bc15d047007257f23d85458dc7561bada4db02
David Rasuli <68276145+DavidRasuli@users.noreply.github.com>
2024-12-17 19:11:13 +0200

    Update Mul.java

--- Mul.java
+++ Mul.java
@@ -6,6 +6,10 @@ public class Mul extends BinaryExpression {
 		super(left, right);
 	}
 
+	public Mul(double left, double right){
+		super(left,right);
+	}
+	
 	@Override
 	public double calculate() {
 		return left.calculate()*right.calculate();
