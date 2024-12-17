6746005dd0a6ce8adce8f4fceb82059b0c237837
David Rasuli <68276145+DavidRasuli@users.noreply.github.com>
2024-12-17 19:10:26 +0200

    Update Div.java

--- Div.java
+++ Div.java
@@ -6,6 +6,10 @@ public class Div extends BinaryExpression {
 		super(left, right);
 	}
 
+	public Div(double left, double right){
+		super(left,right);
+	}
+	
 	@Override
 	public double calculate() {
 		return left.calculate()/right.calculate();
